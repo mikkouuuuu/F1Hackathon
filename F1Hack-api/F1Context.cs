6db69cb8b380bee0fac93ca2b8da7857ab6798e0
Mikko Marttila <mikko.marttila@alfame.com>
2023-05-27 13:26:55 +0300

    Context namespace

--- F1Hack-api/F1Context.cs
+++ F1Hack-api/F1Context.cs
@@ -3,37 +3,39 @@ using F1Hack_api.Entities.Identiy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
-public class F1Context : IdentityDbContext<User, IdentityRole<int>, int>
+namespace F1Hack_api
 {
-    public DbSet<Prediction> Predictions { get; set; }
-    public DbSet<PredictionGroup> PredictionGroups { get; set; }
-    public DbSet<PredictionValues> PredictionValues { get; set; }
-
-    public F1Context(DbContextOptions<F1Context> options) : base(options)
+    public class F1Context : IdentityDbContext<User, IdentityRole<int>, int>
     {
+        public DbSet<Prediction> Predictions { get; set; }
+        public DbSet<PredictionGroup> PredictionGroups { get; set; }
+        public DbSet<PredictionValues> PredictionValues { get; set; }
 
-    }
+        public F1Context(DbContextOptions<F1Context> options) : base(options)
+        {
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        builder.Entity<Prediction>()
-            .HasMany(p => p.PredictionValues)
-            .WithOne(p => p.Prediction)
-            .HasForeignKey(p => p.PredictionId);
+        }
 
-        builder.Entity<Prediction>()
-            .HasOne(p => p.PredictionGroup)
-            .WithMany(p => p.Predictions)
-            .HasForeignKey(x => x.PredictionGroupId);
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Prediction>()
+                .HasMany(p => p.PredictionValues)
+                .WithOne(p => p.Prediction)
+                .HasForeignKey(p => p.PredictionId);
 
-        builder.Entity<Prediction>()
-            .HasOne(x => x.User)
-            .WithMany(x => x.Predictions)
-            .HasForeignKey(x => x.UserId);
+            builder.Entity<Prediction>()
+                .HasOne(p => p.PredictionGroup)
+                .WithMany(p => p.Predictions)
+                .HasForeignKey(x => x.PredictionGroupId);
 
-        builder.Entity<IdentityUserLogin<int>>().HasNoKey();
-        builder.Entity<IdentityUserRole<int>>().HasNoKey();
-        builder.Entity<IdentityUserToken<int>>().HasNoKey();
+            builder.Entity<Prediction>()
+                .HasOne(x => x.User)
+                .WithMany(x => x.Predictions)
+                .HasForeignKey(x => x.UserId);
+
+            builder.Entity<IdentityUserLogin<int>>().HasNoKey();
+            builder.Entity<IdentityUserRole<int>>().HasNoKey();
+            builder.Entity<IdentityUserToken<int>>().HasNoKey();
+        }
     }
 }
