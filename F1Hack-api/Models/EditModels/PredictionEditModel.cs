65e3a38b139317413d6991cc7cbef399e5c4f1e6
Mikko Marttila <mikko.marttila@alfame.com>
2023-05-27 10:59:04 +0300

    Added required attribute to editmodels

--- F1Hack-api/Models/EditModels/PredictionEditModel.cs
+++ F1Hack-api/Models/EditModels/PredictionEditModel.cs
@@ -1,10 +1,13 @@
 ﻿using F1Hack_api.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace F1Hack_api.Models
 {
     public class PredictionEditModel
     {
+        [Required]
         public int UserId { get; set; }
+        [Required]
         public int PredictionGroupId { get; set; }
         public IEnumerable<PredictionValuesEditModel> PredictionValues { get; set; }
 
--- F1Hack-api/Models/EditModels/PredictionValuesEditModel.cs
+++ F1Hack-api/Models/EditModels/PredictionValuesEditModel.cs
@@ -1,11 +1,14 @@
 ﻿using F1Hack_api.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace F1Hack_api.Models
 {
     public class PredictionValuesEditModel
     {
         public int PredictionId { get; set; }
+        [Required]
         public string DescribingValue { get; set; }
+        [Required]
         public string PredictionValue { get; set; }
 
         public PredictionValues ToEntity()
